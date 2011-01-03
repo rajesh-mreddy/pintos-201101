c208859f611c3958ca114827569ca4a65ebbcfa0
williamchai2006 <williamchai2006@f7ab8d83-5263-4f91-ed9b-c8a5b253c69c>
2011-01-03 14:40:13 +0000

    init system_status lost a line of code :p

--- branches/pintos-w/src/threads/thread.c
+++ branches/pintos-w/src/threads/thread.c
@@ -106,6 +106,9 @@ thread_init (void)
   init_thread (initial_thread, "main", PRI_DEFAULT);
   initial_thread->status = THREAD_RUNNING;
   initial_thread->tid = allocate_tid ();
+
+	/* change system_status in kernel mode, will changed in init.c */
+	set_system_status(SYSTEM_KERNEL);
 }
 
 /* Starts preemptive thread scheduling by enabling interrupts.
